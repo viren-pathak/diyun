0daa8230251085dca300c52e1ef36884239c5f3c
apple <engr.deepak.pathak@gmail.com>
2024-05-02 11:08:47 +0530

    Basic Login Signup Form

---
+++ resources/views/home.blade.php
@@ -0,0 +1,4 @@
+@extends('header')
+@section('content')
+<h1>DIYUN</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('dashboard', [UserController::class, 'dashboard'])->name('dashboard');
+Route::get('login', [UserController::class, 'index'])->name('login');
+Route::post('custom-login', [UserController::class, 'customLogin'])->name('login.custom');
+Route::get('register', [UserController::class, 'registration'])->name('register');
+Route::post('custom-registration', [UserController::class, 'customRegistration'])->name('register.custom');
+Route::get('signout', [UserController::class, 'signOut'])->name('signout');
