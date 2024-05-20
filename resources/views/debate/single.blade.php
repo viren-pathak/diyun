b316206602dad865047fa27f0285ac64c9d744ed
apple <engr.deepak.pathak@gmail.com>
2024-05-20 12:59:37 +0530

    Structurize claim cards

--- resources/views/debate/single.blade.php
+++ resources/views/debate/single.blade.php
@@ -10,6 +10,18 @@
         <div class="content__body-container">
             <div class="content__body">
                 <div class="ancestor-stack">
+                    <div class="ancestor-claim-container">
+                        <div class="ancestor-claim">
+                            <div class="claim-card">
+                                <div class="claim-header">
+                                </div>
+
+                                <div class="claim-text">
+                                    <h2 class="claim-text__content">{{ $debate->thesis }}</h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <main class="content__selected-and-columns">
                     <div class="selected-claim-container">
@@ -27,7 +39,7 @@
 
                     <div class="columns-container">
                         <div class="columns-container__column-headers">
-                            <div class="column-box column-box--pro">
+                            <div class="column-box column-box--header--pro">
                                 <p class="column-box--pro--info">Pros</p>
                                 <button type="button" class="btn-danger btn add-pro-btn" data-authenticated="{{ auth()->check() }}">+</button>
                                 <div class="add-pro-form" style="display:none;">
@@ -39,16 +51,9 @@
                                         <button type="button" class="close-form-btn">Close</button>
                                     </form>
                                 </div>
-                                <div class="pros">
-                                    @foreach($pros as $pro)
-                                        <div class="pro-argument">
-                                            {{ $pro->title }}
-                                        </div>
-                                    @endforeach
-                                </div>
                             </div>
 
-                            <div class="column-box column-box--con">
+                            <div class="column-box column-box--header--con">
                                 <p class="column-box--con--info">Cons</p>
                                 <button type="button" class="btn-danger btn add-cons-btn" data-authenticated="{{ auth()->check() }}">+</button>
                                 <div class="add-con-form" style="display:none;">
@@ -60,18 +65,50 @@
                                         <button type="button" class="close-form-btn">Close</button>
                                     </form>
                                 </div>
-                                <div class="cons">
-                                    @foreach($cons as $con)
-                                        <div class="con-argument">
-                                            {{ $con->title }}
-                                        </div>
-                                    @endforeach
-                                </div>
                             </div>
                         </div>
 
                         <div class="columns-container__column-contents">
+                            <div class="column-box column-box--claims column-box--claims-pro">
+                                <ul class="column__content--pros">
+                                    @foreach($pros as $pro)
+                                        <li class="pro-argument">
+                                            <div class="actionable-claim-container">
+                                                <div class="actionable-claim">
+                                                    <div class="claim-card child-claim-card">
+                                                        <div class="claim-header">
+                                                        </div>
 
+                                                        <div class="claim-text">
+                                                            <p class="claim-text__content">{{ $pro->title }}</p>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                            <div class="column-box column-box--claims column-box--claims-con">
+                                <ul class="column__content--cons">
+                                    @foreach($cons as $con)
+                                        <li class="con-argument">
+                                            <div class="actionable-claim-container">
+                                                <div class="actionable-claim">
+                                                    <div class="claim-card child-claim-card">
+                                                        <div class="claim-header">
+                                                        </div>
+
+                                                        <div class="claim-text">
+                                                            <p class="claim-text__content">{{ $con->title }}</p>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </li>
+                                    @endforeach
+                                </ul>
+                            </div>
                         </div>
                     </div>
 
